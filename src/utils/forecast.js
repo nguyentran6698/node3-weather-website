8db4447e8286421f716542b6f465f122613d39eb
nguyentran <nguyen.tran6698@gmail.com>
2020-12-30 21:02:49 -0600

    Adding humidity in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude,longtitude, callback) => {
             callback('Unable to find location!!!',undefined);
         }
         else{
-            callback(undefined,body.current.weather_descriptions[0]+`. It is ${body.current.temperature} temperature, but it feels like ${body.current.feelslike} `)
+            callback(undefined,body.current.weather_descriptions[0]+`. It is ${body.current.temperature} temperature, but it feels like ${body.current.feelslike}. 
+                    The humidity of the location: ${body.current.humidity}`)
         }
     })
 }
